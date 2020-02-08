b2dd895c73e1337ce031d3eb4593e9a725a5ce7f
OldTaoge <51744784+OldTaoge@users.noreply.github.com>
2020-02-08 20:52:53 +0800

    Update ajax.py

--- xxtj/api/ajax.py
+++ xxtj/api/ajax.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 
 
 def query_description_by_statistics(statistics):
-    if statistics > 0:
-        sta = statistics_information.objects.get(pk=statistics)
-        description = sta.statistics_description
-        return HttpResponse(description)
-    else:
-        return HttpResponse('请在 选择项目 中选择您要进行提交的项目')
+    sta = statistics_information.objects.get(pk=statistics)
+    description = sta.statistics_description
+    return HttpResponse(description)
