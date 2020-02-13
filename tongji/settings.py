967025b13c41bc398f527e95f101324ba06be4b1
OldTaoge <duwentao2006@gamil.com>
2020-02-13 17:27:50 +0800

    information

--- tongji/settings.py
+++ tongji/settings.py
@@ -84,7 +84,8 @@ DATABASES = {
         'PASSWORD': "dumywensqltao",
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        },
+        'CONN_MAX_AGE': 5*60,
     },
 
 }
